d10b6e70597fd3416b2af2eaa3ca9d5dffb85250
david <d8087869@gmail.com>
2026-06-23 11:50:41 +0300

    added3_5

--- database.py
+++ database.py
@@ -1,5 +1,14 @@
 # 1
-agent = {'name':'alpha', 'level':3, 'active': True}
+agent = {'name':'alpha', 'level': 3, 'active': True}
 print(agent)
 #2 
 print(agent['name'])
+#3 
+if agent['level']:
+    print(agent.get('level'))
+else:
+    print(0)
+agent['score']=95   
+print(agent)
+agent['level']=5
+print(agent)
